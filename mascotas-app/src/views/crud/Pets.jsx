e6957e230c4aca228e405ec0bcdde4ec8f714bd2
ProgrammerAuditore <victorvj098@gmail.com>
2022-05-10 18:42:16 -0500

    *** dev : Se agrego card para mostrar las mascotas

--- mascotas-app/src/views/crud/Pets.jsx
+++ mascotas-app/src/views/crud/Pets.jsx
@@ -1,10 +1,87 @@
 import React, { useState, useEffect } from 'react';
+import MascotaService from '../../service/MascotaService';
+
 function Pets() {
+    const [mascotas, setMascota] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            await MascotaService.getMascotas()
+            .then((resp) => {
+                let listaMascotas = resp.data.data;
+                setMascota(listaMascotas);
+            })
+            .catch((resp) => {
+                window.location.href = "/";
+            });
+        }
+
+        fetchData();
+    }, []);
+
+    const fncBtnVer = (_id) => {
+        window.location.href = `/view/${_id}`;
+    };
+
+    const fncBtnEditar = (_id) => {
+        window.location.href = `/update/${_id}`;
+    };
+
+    const fncBtnEliminar = (_id) => {
+        MascotaService.deleteMascota(_id);
+        window.location.reload();
+    };
+
+  return (
+    <>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+{ Object.keys(mascotas).map( (index) => {
+    let pet = mascotas[index];
     return (<>
-        <div>
-            <h1>Page Pets</h1>
+        <div class="col" key={index}>
+            <div class="card h-100">
+            {/* <img src="..." class="card-img-top" alt="..."/> */}
+            <div class="card-body">
+                <h5 class="card-title">{pet.nombre}</h5>
+                <small class="card-text text-muted">{pet.raza} con {pet.edad} años de edad. <br/> Además cuenta con las siguientes enfermedades: </small>
+                <ul class="list-group">
+                { pet.enfermedades.map( (el) => {
+                    return (<li class="list-group-item list-group-item-primary" key={index}>
+                        {el}
+                    </li>);
+                }) }
+                </ul>
+            </div>
+            <div class="card-footer">
+                <div class="d-flex justify-content-between">
+                    <small class="text-muted">Creado hace {new Date().getHours() - new Date(pet.createdAt).getHours()} hrs.</small>
+                    {/* Acciones para el modal */}
+                <div class="btn-group btn-group-sm" role="group" aria-label="...">
+                    <a href={"#"} onClick={()=> fncBtnVer(pet._id)} className="btn btn-dark btn-sm btn-block">
+                        <i class="fas fa-eye"></i>
+                    </a> 
+                    <a href={"#"} onClick={()=> fncBtnEditar(pet._id)} className="btn btn-warning">
+                        <i class="fas fa-edit"></i>
+                    </a> 
+                    <a href={"#"} onClick={()=> fncBtnEliminar(pet._id)} className="btn btn-danger">
+                        <i class="fas fa-trash"></i>
+                    </a>
+                </div> 
+                </div>
+            </div>
+            </div>
         </div>
-    </> );
+    </>);
+})}
+
+  <div class="col">
+    
+  </div>
+  
+</div>
+
+    </>
+  );
 }
 
 export default Pets;
