ced1e08c8e9aa9f11e92e68bf3f56142000bdb5d
ProgrammerAuditore <victorvj098@gmail.com>
2022-05-10 18:44:47 -0500

    *** dev : Se agrego un menu desplegable

--- mascotas-app/src/views/partials/NavBar.jsx
+++ mascotas-app/src/views/partials/NavBar.jsx
@@ -15,9 +15,18 @@ function NavBar() {
                 <li className="nav-item">
                     <a className="nav-link active" aria-current="page" href="/">Inicio</a>
                 </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/add">Agregar</a>
-                </li>
+                
+               
+                <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Mis mascotas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="/pets">Ver mascotas</a></li> 
+            <li><hr class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="/add">Registrar</a></li>
+          </ul>
+        </li>
             </ul>
             <form className="d-flex">
                 <input className="form-control me-2" type="search" placeholder="Buscar una mascota..." aria-label="Buscar"/>
